e8374e0a5bf4acc9fcb74aa0f977a535f7819141
maherparkar <maherparkar@gmail.com>
2025-09-07 21:08:41 +1000

    Initial Commit

---
+++ task_managerTests/task_managerTests.swift
@@ -0,0 +1,17 @@
+//
+//  task_managerTests.swift
+//  task_managerTests
+//
+//  Created by Maher Parkar on 7/9/2025.
+//
+
+import Testing
+@testable import task_manager
+
+struct task_managerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
